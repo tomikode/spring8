365c995e896e501614b797bef9f28d7115a90c33
tomikode <tomi.piisila@gmail.com>
2022-08-27 08:56:50 +1000

    test for sqlite

--- src/main/java/example/test/TestController.java
+++ src/main/java/example/test/TestController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
     
+// https://www.youtube.com/watch?v=x9GaBXH0H4s
+// https://www.youtube.com/watch?v=KTBWCJPKiqk
+// https://andriperera.medium.com/spring-boot-restful-application-with-cloud-sql-for-mysql-7bfad7b3824b
 
     @GetMapping
     String tests(Model model) {
